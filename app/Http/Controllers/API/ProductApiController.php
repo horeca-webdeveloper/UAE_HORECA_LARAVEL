4e51977ded096c3b6d33ac0427c75d10e4823bfb
Ashar-khursheed <asharkhursheed26@gmail.com>
2025-06-11 19:11:22 +0500

    cgad

--- app/Http/Controllers/API/ProductApiController.php
+++ app/Http/Controllers/API/ProductApiController.php
@@ -55,7 +55,7 @@ class ProductApiController extends Controller
                 }
 
                 // Start building the base query
-                $query = Product::with(['categories', 'brand', 'tags', 'producttypes'])
+                $query = Product::with(['categories', 'brand' , 'brand.products.reviews'])
                     ->where('status', 'published');
 
                 // Apply filters
@@ -155,6 +155,26 @@ class ProductApiController extends Controller
                         if (is_string($product->description)) {
                             $product->description = json_decode($product->description, true);
                         }
+
+                        if ($product->brand) {
+                            $product->brand_id = $product->brand->id;
+                            $product->brand_name = $product->brand->name;
+                            $product->brand_logo = $product->brand->logo;
+                        
+                            // Get all reviews of all products under this brand
+                            $brandReviews = $product->brand->products->flatMap(function ($p) {
+                                return $p->reviews;
+                            });
+                        
+                            $brandReviewCount = $brandReviews->count();
+                            $brandAvgRating = $brandReviewCount > 0
+                                ? round($brandReviews->avg('star'), 1)
+                                : null;
+                        
+                            $product->brand_avg_rating = $brandAvgRating;
+                            $product->brand_review_count = $brandReviewCount;
+                        }
+                        
 
 
                         // // Handle images
@@ -395,8 +415,8 @@ class ProductApiController extends Controller
                         }
 
                         // Add tags and types
-                        $product->tags = $product->tags;
-                        $product->producttypes = $product->producttypes;
+                        // $product->tags = $product->tags;
+                        // $product->producttypes = $product->producttypes;
                         $product->category_list = $product->categories->map(function ($category) {
                             return [
                                 'id' => $category->id,
@@ -813,7 +833,7 @@ class ProductApiController extends Controller
         }
 
         // Start building the base query
-        $query = Product::with(['categories', 'brand', 'tags', 'producttypes'])
+        $query = Product::with(['categories', 'brand' , 'brand.products.reviews'])
             ->where('status', 'published');
 
         // Apply filters
@@ -930,6 +950,27 @@ class ProductApiController extends Controller
                         $product->benefit_features = [];
                     }
                 }
+
+                
+                if ($product->brand) {
+                    $product->brand_id = $product->brand->id;
+                    $product->brand_name = $product->brand->name;
+                    $product->brand_logo = $product->brand->logo;
+                
+                    // Get all reviews of all products under this brand
+                    $brandReviews = $product->brand->products->flatMap(function ($p) {
+                        return $p->reviews;
+                    });
+                
+                    $brandReviewCount = $brandReviews->count();
+                    $brandAvgRating = $brandReviewCount > 0
+                        ? round($brandReviews->avg('star'), 1)
+                        : null;
+                
+                    $product->brand_avg_rating = $brandAvgRating;
+                    $product->brand_review_count = $brandReviewCount;
+                }
+                
 
             // Select only required fields for the response
             // $product->images = collect($product->images)->map(function ($image) {
